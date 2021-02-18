fdbb27598d561c2eab65d545c75dad9a386901c2
LukeSunner <77940113+LukeSunner@users.noreply.github.com>
2021-02-18 23:55:17 +0000

    Movement on the ball
    The ball now has movement which was scripted. I also made a order around to test the ball movement.

---
+++ Golf Game/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField]private float shotPower, maxForce, minSpeed;
+    
+    private Rigidbody myRB;
+    private float shotForce;
+    private Vector3 startPos, endPos, direction;
+    private bool canShoot, shotStarted;
+
+    private void Start() 
+    { 
+        myRB = GetComponent<Rigidbody>();
+        canShoot = true;
+        myRB.sleepThreshold = minSpeed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            startPos = MousePos();
+            shotStarted = true;
+        }
+        if (Input.GetMouseButton(0) && shotStarted)
+        {
+            endPos = MousePos();
+            shotForce = Mathf.Clamp(Vector3.Distance(endPos, startPos), 0, maxForce);
+        }
+        if (Input.GetMouseButtonUp(0) && shotStarted)
+        {
+            canShoot = false;
+            shotStarted = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canShoot)
+        {
+            direction = startPos - endPos;
+            myRB.AddForce(Vector3.Normalize(direction )* shotForce * shotPower, ForceMode.Impulse);
+            startPos = endPos = Vector3.zero;
+        }
+
+        if (myRB.IsSleeping())
+        {
+            canShoot = true;
+        }
+    }
+
+    private Vector3 MousePos()
+        {
+            Vector3 position = Vector3.zero;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                position = hit.point;
+            }
+
+            return position;
+        }
+    
+}
